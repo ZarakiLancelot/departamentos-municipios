f58a19f21efdd5fd82355a217f45510f1e40cf39
ZarakiLancelot <kenpachi2783@gmail.com>
2023-07-31 03:53:11 -0600

    CORS para localhost y nueva estructura de JSON

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const datosJson = require('./datos.json');
 
+// Middleware para habilitar CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  next();
+});
+
 // Ruta del servicio que devuelve el JSON estático
 app.get('/api/datos', (req, res) => {
   res.json(datosJson);
